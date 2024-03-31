8c056e2fd89eebf5f56962d056db16fde66f4dc4
tahfeezmizan <fullname@gmail.com>
2024-04-01 00:58:55 +0600

    create home, login. register route

--- src/components/Header/Login.jsx
+++ src/components/Header/Login.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-           <h1>Login Here</h1> 
+        <div className="hero min-h-screen bg-base-200">
+            <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <h1 className="text-5xl font-bold pt-10">Login now!</h1>
+                <form className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control my-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                    <NavLink to='/register' className='btn bg-green-600 text-white'>Create an Account</NavLink>
+                </form>
+                
+            </div>
         </div>
     );
 };
--- src/components/Header/Register.jsx
+++ src/components/Header/Register.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Register = () => {
+
+    const handleUserRegister = e => {
+        e.preventDefault();
+
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password)
+    }
+
     return (
-        <div>
-            <h1>Register</h1>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col gap-10 px-44 lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Register now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onClick={handleUserRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="name" name='name' placeholder="Your Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="Enter You Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="Enter You Password" className="input input-bordered" required />
+                        </div>
+
+                        <div className="flex items-center gap-3">
+                            <input type="checkbox" name="terms" id="trams" />
+                            <label htmlFor="trams">accept our <NavLink className='text-green-300'>terms & condition</NavLink></label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                        <p>already have account <NavLink to="/login" className="underline text-green-400">login</NavLink></p>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
